1a56e95e9c392af2585fc3f603a5a64b3270e878
Nanotect <wilferspaepen@gmail.com>
2022-01-19 12:58:11 +0100

    change result enum

--- GreenHealth-API-backend/Models/Result.cs
+++ GreenHealth-API-backend/Models/Result.cs
@@ -11,14 +11,16 @@ namespace GreenHealth_API_backend.Models
 		public int Id { get; set; }
 #nullable disable
 		public double Accuracy { get; set; }
-		public enum GrowthStage
-		{
-			week1,
-			week2,
-			week3,
-			week4,
-			week5,
-			week6
-		}
+		public GrowthStage GrowthStage { get; set; }
+	}
+
+	public enum GrowthStage
+	{
+		week1,
+		week2,
+		week3,
+		week4,
+		week5,
+		week6
 	}
 }
