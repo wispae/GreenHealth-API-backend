adb05ce24ed7533bc7ba8260afc210e2c37cf795
Nanotect <wilferspaepen@gmail.com>
2022-01-19 19:56:07 +0100

    fix merge problems

--- GreenHealth-API-backend/Data/DbInitializer.cs
+++ GreenHealth-API-backend/Data/DbInitializer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GreenHealth_API_backend.Models;
 
 namespace GreenHealth_API_backend.Data
 {
@@ -10,6 +11,82 @@ namespace GreenHealth_API_backend.Data
 		public static void Initialize(DataContext context)
 		{
 			context.Database.EnsureCreated();
+
+			// Look for any users.
+			if (context.User.Any())
+			{
+				return;   // DB has been seeded
+			}
+
+			// Add results
+
+			context.AddRange(
+				new Result { Accuracy = 92.6, GrowthStage = GrowthStage.week1 },
+				new Result { Accuracy = 91.2, GrowthStage = GrowthStage.week2 },
+				new Result { Accuracy = 87.98, GrowthStage = GrowthStage.week3 },
+				new Result { Accuracy = 93, GrowthStage = GrowthStage.week4 },
+				new Result { Accuracy = 997.009, GrowthStage = GrowthStage.week5 },
+				new Result { Accuracy = 95.1, GrowthStage = GrowthStage.week6 }
+				);
+
+			//Add users
+
+			context.AddRange(
+				new User { FirstName = "Brent", LastName = "Van de Staey", Email = "brent@vandestaey.be", Password = "Brent1234", IsAdmin = true },
+				new User { FirstName = "Toon", LastName = "De Bie", Email = "toon@debie.be", Password = "Toon1234", IsAdmin = false },
+				new User { FirstName = "Wilfer", LastName = "Spaepen", Email = "Wilfer@spaepen.be", Password = "Wilfer1234", IsAdmin = false }
+				);
+
+			context.SaveChanges();
+
+			// Add Plants
+
+			Plant plant1 = new Plant()
+			{
+				UserId = 1,
+				ResultId = 1,
+			};
+
+			Plant plant2 = new Plant()
+			{
+				UserId = 1,
+				ResultId = 2,
+			};
+
+			Plant plant3 = new Plant()
+			{
+				UserId = 2,
+				ResultId = 3,
+			};
+
+			Plant plant4 = new Plant()
+			{
+				UserId = 2,
+				ResultId = 4,
+			};
+
+			Plant plant5 = new Plant()
+			{
+				UserId = 3,
+				ResultId = 5,
+			};
+
+			Plant plant6 = new Plant()
+			{
+				UserId = 3,
+				ResultId = 6,
+			};
+
+			context.Add(plant1);
+			context.Add(plant2);
+			context.Add(plant3);
+			context.Add(plant4);
+			context.Add(plant5);
+			context.Add(plant6);
+
+			context.SaveChanges();
+
+			//success!
 		}
 	}
 }
