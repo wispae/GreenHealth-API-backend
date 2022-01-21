af09018d0e7cceda0d9a20d960703882542a6d38
Brentvds12 <r0793295@student.thomasmore.be>
2022-01-21 11:05:12 +0100

    added security via tokens

---
+++ GreenHealth-API-backend/Controllers/UserAuthController.cs
@@ -0,0 +1,29 @@
+﻿using GreenHealth_API_backend.Models;
+using GreenHealth_API_backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenHealth_API_backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Email, userParam.Password);
+            if (user == null)
+                return BadRequest(new { message = "Email or password is incorrect" });
+            return Ok(user);
+        }
+    }
+}
--- GreenHealth-API-backend/Services/UserService.cs
+++ GreenHealth-API-backend/Services/UserService.cs
@@ -1,20 +1,28 @@
 ﻿using GreenHealth_API_backend.Data;
+using GreenHealth_API_backend.Helpers;
 using GreenHealth_API_backend.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GreenHealth_API_backend.Services
 {
     public class UserService : IUserService
     {
+        private readonly AppSettings _appSettings;
         private readonly DataContext _context;
 
-        public UserService(DataContext context)
+        public UserService(DataContext context, IOptions<AppSettings> appSettings)
         {
             _context = context;
+            _appSettings = appSettings.Value;
         }
 
         public async Task<IEnumerable<User>> GetUsers()
@@ -74,6 +82,32 @@ namespace GreenHealth_API_backend.Services
         private bool UserExists(int id)
         {
             return _context.User.Any(e => e.Id == id);
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            var user = _context.User.SingleOrDefault(x => x.Email == username && x.Password == password);
+            // return null if user not found
+            if (user == null)
+                return null;
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim("Id", user.Id.ToString()),
+                new Claim("Email", user.Email)
+            }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+            // remove password before returning
+            user.Password = null;
+            return user;
         }
     }
 }
